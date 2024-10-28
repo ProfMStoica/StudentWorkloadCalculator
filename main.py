6195bd3a21deb1d9b05bc01ea639afeae2fd55e1
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-10-28 12:12:18 -0400

    Implement interactivity to ask the user for input and use it to change object data. Use the object to ask it to  perform business logic.

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 """Main module that starts the program"""
+from program import Program
+
+prog = Program()
+
+prog.run()
--- program.py
+++ program.py
@@ -1,3 +1,31 @@
 """User interactivity"""
+from studentworkloadcalculator import  StudentWorkloadCalculator
 class Program:
-    pass
+    #TODO: define user name as a field variable in this class
+
+    def run(self):
+        #TODO: ask the user for their name and initialize field variable with the name provided
+
+        #create a workload calculator object
+        calc = StudentWorkloadCalculator()
+
+        #ask the user for the number of class hours per week
+        classHours = int(input("Please enter the number of class  hours per week: "))
+
+        #set the class hours in the calculator
+        calc.setClassHours(classHours)
+
+        #ask the user for the number of work hours per week
+        workHours  = int(input("Please enter the number of hours per week: "))
+
+        #set the work hours in the calculator
+        calc.setWorkHours(workHours)
+
+        #TODO: use the name the user in the output messages
+        
+        #inform the user how many hours of independent study are required
+        print(f"You will need to study independently for {calc.getIndepStudyHours()}")
+
+        #let the user know what the total workload is
+        totalWorkload = calc.calculateWorkload()
+        print (f"Your total workload per week is {totalWorkload}")
