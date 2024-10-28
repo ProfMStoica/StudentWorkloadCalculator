17aab20dce938f663b56884dd7a21d1624be83b8
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2024-10-28 12:00:42 -0400

    Implement the workload calculator class

--- studentworkloadcalculator.py
+++ studentworkloadcalculator.py
@@ -1,3 +1,25 @@
 """Defines the Student Workload Calculator class"""
 class StudentWorkloadCalculator:
-    pass
+    def __init__(self):
+        self._classHours = 20
+        self._workHours = 10
+
+    def getClassHours(self):
+        return self._classHours
+    
+    def setClassHours(self, newClassHours):
+        self._classHours = newClassHours
+
+    def getWorkHours(self):
+        return self._workHours
+    
+    def setWorkHours(self, newWorkHours):
+        self._workHours = newWorkHours
+    
+    def getIndepStudyHours(self):
+        return self._classHours * 2
+    
+    def calculateWorkload(self):
+        return self._classHours + self.getIndepStudyHours() + self._workHours
+
+    
